ce11e1503f9799594319031351fcbfe38852d9ef
beltranrengifo <beltran@rengifo.es>
2020-11-06 09:48:00 +0100

    config files

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store
+    oneOfsMap.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          resources: [
+            'src/scss/settings/_mixins.scss',
+          ],
+        })
+        .end()
+    })
+  },
 }
