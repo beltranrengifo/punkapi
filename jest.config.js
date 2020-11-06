32ee29382be5ae9c060cd486431f0577c7e4c9a1
beltranrengifo <beltran@rengifo.es>
2020-11-06 09:46:11 +0100

    feat: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
+const path = require('path')
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**/node_modules/**',
+    '!src/store/store.js',
+    '!src/reports',
+  ],
+  snapshotSerializers: ['jest-serializer-vue'],
+  coverageDirectory: path.resolve(__dirname, 'reports/unit/coverage'),
+  verbose: true,
 }
