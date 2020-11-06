37d0f8116af6fc6fac4c839f3696d3139241cb46
beltranrengifo <beltran@rengifo.es>
2020-11-06 09:46:41 +0100

    feat: app component first draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">hi</div>
+  <v-app>
+    <pa-header />
+    <pa-drawer />
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid></v-container>
+    </v-main>
+
+    <v-footer app>
+      <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import PaHeader from '@/components/PaHeader'
+import PaDrawer from '@/components/PaDrawer'
+
 export default {
   name: 'App',
-  components: {},
+
+  components: {
+    PaHeader,
+    PaDrawer,
+  },
 
   data() {
     return {
       DEFAULT_TITLE: 'PunkApi Catalogue',
+      pageNum: 1,
     }
   },
 
-  created() {
+  async created() {
     this.setTitle()
+    await this.fetchBeers({
+      pageNum: this.pageNum,
+    })
   },
 
   methods: {
     setTitle() {
       document.title = this.DEFAULT_TITLE
     },
+
+    ...mapActions({
+      fetchBeers: 'punkapi/fetchBeers',
+    }),
   },
 }
 </script>
-
-<style lang="scss"></style>
