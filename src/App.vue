a1e56f3c73a74d34b2d8d236eb615cf3acb9a6fd
beltranrengifo <beltran@rengifo.es>
2020-11-06 11:40:51 +0100

    feat: theme selector in component

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
     </v-main>
 
     <v-footer app>
-      <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
-        <v-icon>mdi-theme-light-dark</v-icon>
-      </v-btn>
+      <pa-theme-switcher />
     </v-footer>
   </v-app>
 </template>
@@ -19,6 +17,7 @@
 import { mapActions } from 'vuex'
 import PaHeader from '@/components/PaHeader'
 import PaDrawer from '@/components/PaDrawer'
+import PaThemeSwitcher from '@/components/PaThemeSwitcher'
 
 export default {
   name: 'App',
@@ -26,19 +25,12 @@ export default {
   components: {
     PaHeader,
     PaDrawer,
-  },
-
-  data() {
-    return {
-      pageNum: 1,
-    }
+    PaThemeSwitcher,
   },
 
   async created() {
     this.setTitle()
-    await this.fetchBeers({
-      pageNum: this.pageNum,
-    })
+    await this.fetchBeers()
   },
 
   methods: {
---
+++ src/components/PaThemeSwitcher.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pa-theme-switcher">
+    <v-btn icon @click="handleTheme">
+      <v-icon>mdi-theme-light-dark</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaThemeSwitcher',
+
+  methods: {
+    handleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
