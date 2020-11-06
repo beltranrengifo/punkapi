1427954cf61458ecb668ac67b71d0320b35b8f4a
beltranrengifo <beltran@rengifo.es>
2020-11-06 16:46:23 +0100

    feat: main component

---
+++ src/components/PaMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <pa-grid :items="beers" />
+  </main>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import PaGrid from '@/components/PaGrid'
+
+export default {
+  name: 'PaMain',
+
+  components: {
+    PaGrid,
+  },
+
+  async created() {
+    await this.fetchBeers()
+  },
+
+  computed: {
+    ...mapGetters({
+      beers: 'punkapi/getBeers',
+    }),
+  },
+
+  methods: {
+    ...mapActions({
+      fetchBeers: 'punkapi/fetchBeers',
+    }),
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
