427c5f23abec0479bd4e521b623b739fad2ae0e8
beltranrengifo <beltran@rengifo.es>
2020-11-07 11:26:04 +0100

    feat: http module using params object

--- src/store/modules/punkapi.js
+++ src/store/modules/punkapi.js
@@ -18,7 +18,24 @@ const getters = {
 const actions = {
   async fetchBeers({ commit, state }) {
     try {
-      const { data } = await http().get(`/beers?page=${state.pageNum}&per_page=20`)
+      const params = {
+        page: state.pageNum,
+        per_page: 20,
+      }
+      const { data } = await http().get('/beers', { params })
+      commit('SET_BEERS', data)
+    } catch (e) {
+      console.error(e)
+      commit('SET_ERROR', e)
+    }
+  },
+
+  async fetchBeersByName({ commit }, query) {
+    try {
+      const params = {
+        beer_name: query.replace(/\s/, '_', 'g'),
+      }
+      const { data } = await http().get('/beers', { params })
       commit('SET_BEERS', data)
     } catch (e) {
       console.error(e)
