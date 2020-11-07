80a6d6e3e05234735d13f6a07b93741083d786dc
beltranrengifo <beltran@rengifo.es>
2020-11-07 11:28:21 +0100

    feat: search component

---
+++ src/components/PaSearch.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="pa-search">
+    <v-text-field
+      v-model="query"
+      :label="label"
+      clearable
+      filled
+      rounded
+      dense
+      height="20px"
+      color="var(--color-primary)"
+      :hint="hint"
+      @input="handleSearch"
+    />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'PaSearch',
+
+  props: {
+    searching: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    hint: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data() {
+    return {
+      query: '',
+    }
+  },
+
+  methods: {
+    handleSearch() {
+      if (this.query && this.query.length < 3) {
+        return
+      }
+      if (!this.query) {
+        this.$emit('resetSearch')
+        return
+      }
+      this.$emit('handleSearch', this.query)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
