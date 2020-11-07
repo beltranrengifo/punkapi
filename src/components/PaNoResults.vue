1e179a05d09b8bad8ed6be24ed36840d6d2fbee0
beltranrengifo <beltran@rengifo.es>
2020-11-07 11:29:16 +0100

    feat: no results controlling

---
+++ src/components/PaNoResults.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="pa-no-results text-center">
+    <h2 class="mb-8">
+      {{ text }}
+    </h2>
+    <p>
+      Try again!
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PaNoResults',
+
+  props: {
+    text: {
+      type: String,
+      default: 'Nothing found',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
