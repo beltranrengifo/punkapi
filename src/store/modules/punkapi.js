fc25b5873d8ed35aba7d1bb994be404cf729a46f
beltranrengifo <beltran@rengifo.es>
2020-11-07 17:17:17 +0100

    feat: punkapi store module refactor

--- src/store/modules/punkapi.js
+++ src/store/modules/punkapi.js
@@ -3,6 +3,7 @@ import http from '@/utils/http'
 const state = () => ({
   beers: [],
   pageNum: 1,
+  perPage: 20,
   error: false,
 })
 
@@ -16,30 +17,16 @@ const getters = {
 }
 
 const actions = {
-  async fetchBeers({ commit, state }) {
+  async fetchBeers({ commit, state }, { params } = {
+    page: state.pageNum,
+    per_page: state.perPage,
+  }) {
     try {
-      const params = {
-        page: state.pageNum,
-        per_page: 20,
-      }
       const { data } = await http().get('/beers', { params })
       commit('SET_BEERS', data)
     } catch (e) {
-      console.error(e)
       commit('SET_ERROR', e)
-    }
-  },
-
-  async fetchBeersByName({ commit }, query) {
-    try {
-      const params = {
-        beer_name: query.replace(/\s/, '_', 'g'),
-      }
-      const { data } = await http().get('/beers', { params })
-      commit('SET_BEERS', data)
-    } catch (e) {
       console.error(e)
-      commit('SET_ERROR', e)
     }
   },
 }
