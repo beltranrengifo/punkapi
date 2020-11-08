1e6e52412a4afea80c2bafd8b2d1e2d76fa89b86
beltranrengifo <beltran@rengifo.es>
2020-11-08 11:50:41 +0100

    feat: floating button component

---
+++ src/components/PaFloatingAction.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pa-floating-action" @click="$emit('handle-click')">
+    <v-fab-transition>
+      <v-btn :loading="loading" color="gree" fab large dark bottom left>
+        {{ text }}
+      </v-btn>
+    </v-fab-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaFloatingAction',
+
+  props: {
+    text: {
+      type: String,
+      default: 'Action',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pa-floating-action {
+  position: fixed;
+  bottom: rem(24);
+  right: rem(24);
+  z-index: 9;
+}
+</style>
